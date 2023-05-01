a48297946fa21c4c7565124e094c5d6da800f678
oleksandrsoldatchenko <a.soldatchenko@gmail.com>
2023-05-01 17:16:11 +0300

    hw-6 added

--- src/Screens/PostsScreen/PostsScreen.js
+++ src/Screens/PostsScreen/PostsScreen.js
@@ -1,9 +1,8 @@
 import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 import PostList from "../../NestedScreens/PostList/PostList";
-import Comments from "../../NestedScreens/Comments/Comments";
 import Map from "../../NestedScreens/Map/Map";
-
+import Post from "../../Elements/Post";
 const PostNavigation = createStackNavigator();
 
 const NavigationPosts = () => {
@@ -12,8 +11,8 @@ const NavigationPosts = () => {
       initialRouteName="PostList"
       screenOptions={{ headerShown: false }}
     >
+      <PostNavigation.Screen name="Post" component={Post} />
       <PostNavigation.Screen name="PostList" component={PostList} />
-      <PostNavigation.Screen name="Comments" component={Comments} />
       <PostNavigation.Screen name="Map" component={Map}></PostNavigation.Screen>
     </PostNavigation.Navigator>
   );
