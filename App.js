9549c312d5417ed278e3ebd0c6d46b1ff89ec50a
oleksandrsoldatchenko <a.soldatchenko@gmail.com>
2023-05-01 10:11:04 +0300

    hw-6 added

--- App.js
+++ App.js
@@ -1,14 +1,28 @@
-import { TouchableWithoutFeedback, Keyboard } from "react-native";
 import React from "react";
+import { Provider } from "react-redux";
 import { NavigationContainer } from "@react-navigation/native";
+import { TouchableWithoutFeedback, Keyboard } from "react-native";
+
+import store from "./src/Redux/store";
+
+import AuthChack from "./src/Elements/AuthChack";
+import LoadingScreen from "./src/Elements/Loading";
 import Navigation from "./src/Screens/Navigation/Navigation";
+
 
 export default function App() {
   return (
-    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-      <NavigationContainer>
-        <Navigation />
-      </NavigationContainer>
-    </TouchableWithoutFeedback>
+    <Provider store = { store } >
+      <>
+      <LoadingScreen/>
+      <AuthChack>
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+          <NavigationContainer>
+            <Navigation />
+          </NavigationContainer>
+        </TouchableWithoutFeedback>
+      </AuthChack>
+     </>
+   </Provider>
   );
 }
