86a6ffdfa36c63f46d82c34620ca4edfd6d73760
oleksandrsoldatchenko <a.soldatchenko@gmail.com>
2023-05-01 17:10:45 +0300

    hw-6 added

--- src/Screens/Navigation/Navigation.js
+++ src/Screens/Navigation/Navigation.js
@@ -1,7 +1,6 @@
 import { createStackNavigator } from "@react-navigation/stack";
 
 import LoginScreen from "../LoginScreen/LoginScreen";
-// import CreatePostsScreen from "../CreatePostsScreen/CreatePostsScreen";
 import RegistrationScreen from "../RegistrationScreen/RegistrationScreen";
 import Home from "../Home/Home";
 import PostsNav from "./PostsNav";
@@ -20,10 +19,6 @@ const Navigation = () => {
       <MainStack.Screen name="Login" component={LoginScreen} />
       <MainStack.Screen name="Registration" component={RegistrationScreen} />
       <MainStack.Screen name="Home" component={Home} />
-      {/* <MainStack.Screen
-        name="CreatePostsScreen"
-        component={CreatePostsScreen}
-      /> */}
       <MainStack.Screen name="PostsNav" component={PostsNav} />
       <MainStack.Screen name="CommentsNav" component={CommentsNav} />
       <MainStack.Screen name="ProfileScreen" component={ProfileScreen} />
